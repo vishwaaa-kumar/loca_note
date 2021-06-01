a6ec1653587707c9a1a2c0088db7530ecbda6519
vishwaaa-kumar <72905310+vishwaaa-kumar@users.noreply.github.com>
2021-06-01 16:46:34 +0530

    Update places_list_screen.dart

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -38,7 +38,6 @@ class PlacesListScreen extends StatelessWidget {
                   ) ,
                 ),
                 title: Text(greatPlaces.items[i].title),
-                subtitle: Text(greatPlaces.items[i].location.address),
                 onTap: () {
                   Navigator.of(context).pushNamed(
                     PlaceDetailScreen.routeName,
